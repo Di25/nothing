fd654ce5cd2a173015a4a850cac9a565a67e241b
Diron Polson <97249550+Di25@users.noreply.github.com>
2022-02-06 18:36:01 +0100

    just test

---
+++ index.js
@@ -0,0 +1,20 @@
+$("h1").animate({
+
+    opacity: '1',
+    marginTop: '50',
+
+  },3000, function(){
+    $("span").animate({
+    
+        opacity: '1',
+        marginTop: '-50',
+    
+      },1000,);
+  });
+
+  $(".one_Photo").animate({
+    
+    opacity: '1',
+    marginTop: '-50px',
+
+  },3000,);
